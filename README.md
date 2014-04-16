1ef3b9de1fb22fdc007658e9eaf96490207c2dae
Mike Boers <github@mikeboers.com>
2014-04-15 17:54:29 -0700

    Dynamically pick the right name and requirements

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PillowCase
 
-This Python "package" does nothing but satisfy a [PIL][PIL] v1.1.7 requirement. It does **NOT** provide PIL or [Pillow][Pillow] in any manner whatsoever.
+This Python "package" satisfies a [PIL][PIL] v1.1.7 requirement, and requires [Pillow][Pillow] in such a case.
 
 If installed first, this effectively stops PIL and Pillow from fighting over who is returned from an `import PIL`.
 
--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup
+import os
+
+try:
+    import PIL
+except ImportError:
+    PIL = None
+
+
+has_pil = bool(PIL or os.environ.get('PILLOWCASE_REGISTER'))
+
 
 setup(
     
-    name='PIL',
+    name='pillowcase' if has_pil else 'PIL',
     version='1.1.7',
     description='A stub that satisfies PIL==1.1.7 and stops PIL from fighting with Pillow',
 
     url='http://github.com/mikeboers/PillowCase',
-        
+    
+    install_requires=[] if has_pil else ['pillow'],
+    
     author='Mike Boers',
     author_email='pillowcase@mikeboers.com',
     license='BSD-3',
