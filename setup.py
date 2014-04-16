4c47b25370778bdda373f380853e51721e673e9e
Mike Boers <github@mikeboers.com>
2014-04-16 12:23:12 -0700

    Properly "provide" PIL 1.1.7

--- setup.py
+++ setup.py
@@ -13,12 +13,13 @@ has_pil = bool(PIL or os.environ.get('PILLOWCASE_REGISTER'))
 setup(
     
     name='pillowcase' if has_pil else 'PIL',
-    version='1.1.7',
+    version='1.1.8',
     description='A stub that satisfies PIL==1.1.7 and stops PIL from fighting with Pillow',
 
     url='http://github.com/mikeboers/PillowCase',
     
     install_requires=[] if has_pil else ['pillow'],
+    provides=['PIL (1.1.7)'],
     
     author='Mike Boers',
     author_email='pillowcase@mikeboers.com',
