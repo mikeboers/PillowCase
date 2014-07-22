7936ed01d22db3150196cf2b2435e106af38f101
Mike Boers <github@mikeboers.com>
2014-07-22 12:00:50 -0700

    Install a stub egg-info for PIL when we have Pillow

--- setup.py
+++ setup.py
@@ -1,28 +1,69 @@
 from setuptools import setup
+from setuptools.command.egg_info import egg_info
+from setuptools.command.install_egg_info import install_egg_info
+from setuptools.command.install import install
+
 import os
 
-try:
-    import PIL
-except ImportError:
-    PIL = None
+
+def detect_installed():
+    """Return (has_something, has_pillow) tuple."""
+    try:
+        import PIL
+        return True, hasattr(PIL, 'PILLOW_VERSION')
+    except ImportError:
+        return False, False
 
 
-has_pil = bool(PIL or os.environ.get('PILLOWCASE_REGISTER'))
+def setup_pil_stub(command):
+    # Run this if we have, or are likely to install pillow.
+    has_something, has_pillow = detect_installed()
+    if not has_something or has_pillow:
+        setup(
+            script_args=[command],
+            name='PIL',
+            version='1.1.7',
+        )
 
+
+class my_install(install):
+    def run(self):
+        install.run(self)
+        setup_pil_stub('install')
+
+class my_egg_info(egg_info):
+    def run(self):
+        egg_info.run(self)
+        setup_pil_stub('egg_info')
+
+class my_install_egg_info(install_egg_info):
+    def run(self):
+        install_egg_info.run(self)
+        setup_pil_stub('install_egg_info')
+
+
+has_something, has_pillow = detect_installed()
 
 setup(
     
-    name='pillowcase' if has_pil else 'PIL',
-    version='1.1.8',
+    name='pillowcase',
+    version='1.2.0',
     description='A stub that satisfies PIL==1.1.7 and stops PIL from fighting with Pillow',
 
     url='http://github.com/mikeboers/PillowCase',
     
-    install_requires=[] if has_pil else ['pillow'],
-    provides=['PIL (1.1.7)'],
+    # Only "require" PIL if it is already installed, otherwise we prefer Pillow.
+    install_requires=['pil'] if (has_something and not has_pillow) else ['pillow'],
     
     author='Mike Boers',
     author_email='pillowcase@mikeboers.com',
     license='BSD-3',
+
+    # Hook to install stubs for PIL.
+    cmdclass={
+        'install': my_install,
+        'egg_info': my_egg_info,
+        'install_egg_info': my_install_egg_info,
+    },
     
 )
